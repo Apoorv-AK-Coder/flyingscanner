9fdb5c615b52ab872333e3aeeb9dc6cfde965bd2
Apoorv Agarwal <150249267+Apoorv-AK-Coder@users.noreply.github.com>
2025-05-30 16:21:47 +0530

    first

---
+++ css/file.js
@@ -0,0 +1,28 @@
+setInterval(mufun, 1000);
+function mufun() {
+  let b1 = document.getElementById("on");
+  let b2 = document.getElementById("off");
+  let view = document.getElementById("view");
+
+  b1.addEventListener("click", function () {
+    if ((view.style.display = "none")) {
+      view.style.display = "block";
+
+    }
+  })
+
+  b2.addEventListener("click", function () {
+    if ((view.style.display = "block")) {
+      view.style.display = "none";
+    }
+  })
+}
+
+const currentPath = window.location.pathname;
+const navLinks = document.querySelectorAll(".navigate nav ul li a");
+navLinks.forEach(link => {
+  const linkPath = new URL(link.href).pathname;
+  if (linkPath === currentPath) {
+    link.classList.add("active");
+  }
+});

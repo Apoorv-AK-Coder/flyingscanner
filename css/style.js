289b09a2aa7e53235af3f5f6f8910a6a06cb00ac
Apoorv Agarwal <150249267+Apoorv-AK-Coder@users.noreply.github.com>
2025-06-21 12:10:11 +0530

    hghgfgf

--- css/style.js
+++ css/style.js
@@ -27,15 +27,6 @@ navLinks.forEach(link => {
   }
 });
 
-window.addEventListener('scroll', () => {
-  const header = document.querySelector('.newheader');
-  if (window.scrollY > 100) { // Adjust the threshold as needed
-    header.classList.add('scrolled');
-  } else {
-    header.classList.remove('scrolled');
-  }
-});
-
 document.querySelectorAll('.faqhead').forEach(head => {
   head.addEventListener('click', () => {
     const detail = head.nextElementSibling; // Get the next sibling (headdetail)
